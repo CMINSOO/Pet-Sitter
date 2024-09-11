e179c5346ab2c1b9fb93dea269f9a589259802e3
chominsoo <soochomin@gmail.com>
2024-09-11 20:42:57 +0900

    feat/DB Config설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from 'configs/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
