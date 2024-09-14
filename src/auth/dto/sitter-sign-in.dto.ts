93665af1c8dbb3da8ae0d187191127c9bc19ce49
chominsoo <soochomin@gmail.com>
2024-09-14 14:40:06 +0900

    fix/strategy

--- src/auth/dto/sitter-sign-in.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { Sitter } from 'src/sitter/entities/sitter.entity';
-
-export class SitterSignInDto extends PickType(Sitter, ['email', 'password']) {}
